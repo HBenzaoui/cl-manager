649ddeebeac11215e54a65f541f982a79f95c88f
Hamza Benzaoui <hamzapdd@gmail.com>
2019-01-12 19:44:00 +0100

    add total balance above using getDreviedState... and reduce function

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -7,8 +7,27 @@ import PropTypes from 'prop-types';
 import Spinner from '../layout/Spinner';
 
 export class Clients extends Component {
+  state = {
+    totalOwed: null
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    const { clients } = props;
+
+    if (clients) {
+      //Add balances
+      const total = clients.reduce((total, client) => {
+        return total + parseFloat(client.balance.toString());
+      }, 0);
+
+      return { totalOwed: total };
+    }
+    return null;
+  }
+
   render() {
     const { clients } = this.props;
+    const { totalOwed } = this.state;
 
     if (clients) {
       return (
@@ -20,7 +39,14 @@ export class Clients extends Component {
                 <i className="fas fa-users" /> Clients{' '}
               </h2>
             </div>
-            <div className="col-md-6" />
+            <div className="col-md-6">
+              <h5 className="text-right text-secondary">
+                Total:{' '}
+                <span className="text-danger">
+                  {parseFloat(totalOwed).toFixed(2)} DA
+                </span>
+              </h5>
+            </div>
           </div>
           <table className="table table-striped table-hover ">
             <thead className="thead-inverse">
